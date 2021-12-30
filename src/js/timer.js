f90bf25e9390c9b1ee3eafb34c25bc59a363471b
Daryna Dudar <keks9ra1286@gmail.com>
2021-12-30 16:54:30 +0200

    Done

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,14 +1,26 @@
+const hoursEl = document.getElementById('timer');
+let offerTime = 2 * 60 * 60;
+
 function countdown() {
-  let offerTime = 2 * 60 * 60;
-
   const offerHours = Math.floor((offerTime / (60 * 60)) % 24);
-  const offerMins = Math.floor((offerTime * 60) % 60);
-  const offerSecs = Math.floor(offerTime % 60);
+  let offerMins = Math.floor((offerTime / 60) % 60);
+  let offerSecs = Math.floor(offerTime % 60);
 
-  const hoursEl = document.getElementById('timer');
+  if (offerMins < 10) {
+    offerMins = `0${offerMins}`;
+  }
+
+  if (offerSecs < 10) {
+    offerSecs = `0${offerSecs}`;
+  }
+
   hoursEl.innerHTML = `${offerHours} : ${offerMins} : ${offerSecs}`;
 
   offerTime -= 1;
+
+  if (offerTime === 0) {
+    offerTime = 0;
+  }
 }
 
 setInterval(countdown, 1000);
@@ -16,43 +28,3 @@ setInterval(countdown, 1000);
 const today = new Date();
 const orderTime = document.getElementById('orderTime');
 orderTime.innerHTML = `${today.getDate()}.${today.getMonth()}.${today.getFullYear()}`;
-
-// function getTimeRemaining(endtime) {
-//   const total = Date.parse(endtime) - Date.parse(new Date());
-//   const seconds = Math.floor((total / 1000) % 60);
-//   const minutes = Math.floor((total / 1000 / 60) % 60);
-//   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-
-//   return {
-//     total,
-//     hours,
-//     minutes,
-//     seconds,
-//   };
-// }
-
-// function initializeClock(id, endtime) {
-//   const clock = document.getElementById('timer');
-//   const hoursSpan = clock.querySelector('.hours');
-//   const minutesSpan = clock.querySelector('.minutes');
-//   const secondsSpan = clock.querySelector('.seconds');
-
-//   function updateClock() {
-//     const t = getTimeRemaining(endtime);
-
-//     hoursSpan.innerHTML = (t.hours).slice(-2);
-//     minutesSpan.innerHTML = (t.minutes).slice(-2);
-//     secondsSpan.innerHTML = (t.seconds).slice(-2);
-
-//     const timeinterval = setInterval(updateClock, 1000);
-
-//     if (t.total <= 0) {
-//       clearInterval(timeinterval);
-//     }
-//   }
-
-//   updateClock();
-// }
-
-// const deadline = new Date(Date.parse(new Date()) + 2 * 60 * 60 * 1000);
-// initializeClock('timer', deadline);
